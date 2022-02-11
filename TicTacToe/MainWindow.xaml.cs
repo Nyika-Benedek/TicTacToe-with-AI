62a730f8a0df44ccc1e75b31ebe6836110a62b75
Nyika-Benedek <fiokbluestacks@gmail.com>
2022-02-11 17:07:39 +0100

    Drawing Xs and Circles on the front-end

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TicTacToe.Entity;
+using TicTacToe.Model;
 
 namespace TicTacToe
 {
@@ -55,11 +56,12 @@ namespace TicTacToe
             DrawNewField();
         }
 
-        public enum Color { GridLine, XLine }
+        private const int CelldSize = 100;
+        public enum LineType { GridLine, XLine }
 
-        private void DrawLine(int x1, int x2, int y1, int y2, Color color) {
+        private void DrawLine(int x1, int x2, int y1, int y2, LineType color) {
             var myLine = new Line();
-            if (color == Color.GridLine)
+            if (color == LineType.GridLine)
             {
                 myLine.Stroke = System.Windows.Media.Brushes.Green;
             }
@@ -78,20 +80,65 @@ namespace TicTacToe
             canvas.Children.Add(myLine);
         }
 
+        private void DrawX(Coordinate coordinate)
+        {
+            int x = coordinate.X;
+            int y = coordinate.Y;
+            //cell's right left corner
+            x = x * 100;
+            y = y * 100;
+            DrawLine(x, x + CelldSize, y, y + CelldSize, LineType.XLine);
+            DrawLine(x, x+CelldSize, y + CelldSize, y, LineType.XLine);
+        }
+
+        private void DrawCirle(Coordinate coordinate)
+        {
+            Ellipse myEllipse = new Ellipse();
+
+            // Describes the brush's color using RGB values.
+            // Each value has a range of 0-255.
+            myEllipse.StrokeThickness = 2;
+            myEllipse.Stroke = Brushes.White;
+
+            // Set the width and height of the Ellipse.
+            myEllipse.Width = CelldSize;
+            myEllipse.Height = CelldSize;
+
+            Canvas.SetLeft(myEllipse, coordinate.X * CelldSize);
+            Canvas.SetTop(myEllipse, coordinate.Y * CelldSize);
+
+            // Add the Ellipse to the StackPanel.
+            canvas.Children.Add(myEllipse);
+        }
+
         private void DrawNewField() {
             canvas.Children.Clear();
             // Vertical Lines
-            DrawLine(100, 100, 0, 300, Color.GridLine);
-            DrawLine(200, 200, 0, 300, Color.GridLine);
+            DrawLine(100, 100, 0, 300, LineType.GridLine);
+            DrawLine(200, 200, 0, 300, LineType.GridLine);
 
             // Horizontal Lines
-            DrawLine(0, 300, 100, 100, Color.GridLine);
-            DrawLine(0, 300, 200, 200, Color.GridLine);
+            DrawLine(0, 300, 100, 100, LineType.GridLine);
+            DrawLine(0, 300, 200, 200, LineType.GridLine);
+        }
+
+        private Coordinate GetMousePosition() {
+            int x = (int)(Mouse.GetPosition(canvas).X / CelldSize);
+            int y = (int)(Mouse.GetPosition(canvas).Y / CelldSize);
+            return new Coordinate(x, y);
+        }
+
+        private void CanvasClick(object sender, MouseButtonEventArgs e)
+        {
+            
+            DrawX(GetMousePosition());
+            DrawCirle(GetMousePosition());
+            
         }
 
         private void NewGame(object sender, RoutedEventArgs e)
         {
-
+            DrawNewField();
         }
 
         private void Query(object sender, RoutedEventArgs e)
@@ -99,9 +146,5 @@ namespace TicTacToe
 
         }
 
-        private void CanvasClick(object sender, MouseButtonEventArgs e)
-        {
-
-        }
     }
 }
---
+++ TicTacToe/Models/Coordinate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Model
+{
+    class Coordinate
+    {
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public Coordinate(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Coordinate);
+        }
+
+        public bool Equals(Coordinate other)
+        {
+            return !(other is null)
+                && this.X == other.X
+                && this.Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.X, this.Y);
+        }
+
+        public static bool operator ==(Coordinate left, Coordinate right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Coordinate left, Coordinate right)
+        {
+            return !(left == right);
+        }
+        public override string ToString()
+        {
+            return "X" + this.X + "Y" + this.Y;
+        }
+
+    }
+}
