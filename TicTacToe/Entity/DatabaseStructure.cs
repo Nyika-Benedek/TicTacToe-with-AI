6e4055cff97da132fca8eed7dd5f1e4621861cb0
Nyika-Benedek <fiokbluestacks@gmail.com>
2022-02-11 15:27:33 +0100

    Add project files.

---
+++ TicTacToe/Entity/DatabaseStructure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Entity
+{
+    public class DatabaseStructure
+    {
+        public int id { get; set; }
+        public DateTime date { get; set; }
+
+
+        public DatabaseStructure()
+        {
+            this.date = DateTime.Now;
+        }
+
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TicTacToe.Entity;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /*
+            DatabaseContextFactory databaseFactory = new DatabaseContextFactory();
+            using var databaseContext = databaseFactory.CreateDbContext(new string[0]);
+
+            // adding entry to database
+            DatabaseStructure entry = new DatabaseStructure();
+            databaseContext.database.Add(entry);
+            databaseContext.SaveChanges();
+            MessageBox.Show($"id:{entry.id} > Entry added!");
+
+            // deleting entry by id
+            databaseContext.Remove( databaseContext.database.Find(1));
+            databaseContext.SaveChanges();
+            */
+
+
+            DatabaseCommands database = new DatabaseCommands();
+
+            database.AddEntry();
+            database.DeleteById(2);
+            database.UpdateById(5);
+
+            foreach (var entry in database.GetAll()) {
+                dataGrid.Items.Add(entry);
+            }
+
+
+        }
+    }
+}
